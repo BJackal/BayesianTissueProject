b1925d2551e91f8ebc075f5267f412eb42b8ea32
Jack <44051158+BJackal@users.noreply.github.com>
2025-08-12 13:14:12 +0100

    Update VertexEdgeLengthWriter.cpp
    Add Immersed Boundary

--- src/VertexEdgeLengthWriter.cpp
+++ src/VertexEdgeLengthWriter.cpp
@@ -40,6 +40,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "NodeBasedCellPopulation.hpp"
 #include "PottsBasedCellPopulation.hpp"
 #include "VertexBasedCellPopulation.hpp"
+#include "ImmersedBoundaryCellPopulation.hpp"
 #include "VertexElement.hpp"
 #include "UblasIncludes.hpp"
 
@@ -71,6 +72,12 @@ template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 void VertexEdgeLengthWriter<ELEMENT_DIM, SPACE_DIM>::Visit(PottsBasedCellPopulation<SPACE_DIM>* pCellPopulation)
 {
   EXCEPTION("This writer is supposed to be used with a VertexBasedCellPopulation only.");
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+void VertexEdgeLengthWriter<ELEMENT_DIM, SPACE_DIM>::Visit(ImmersedBoundaryCellPopulation<SPACE_DIM>* pCellPopulation)
+{
+    EXCEPTION("This writer is supposed to be used with a VertexBasedCellPopulation only.");
 }
 
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
